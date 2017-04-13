05da4bd138f2e160d71a5a61b17a30bd4756f066
Marcel Rösler <roesler1@gmx.de>
2017-04-13 20:59:30 +0200

    more builder, better role allocation

--- src/creeps.js
+++ src/creeps.js
@@ -18,11 +18,11 @@ let creeps = {
             }else{
                 creeps = Object.keys(Game.creeps).map((creepName, index) =>{
                     let sources = Game.creeps[creepName].room.find(FIND_SOURCES);
-                    if(index <= 1){
-                        Game.creeps[creepName].memory.role = "upgrader"
-                        Game.creeps[creepName].memory.source = sources[0]
-                    } else if(index <= amountOfBuilder+2 ){
+                    if(index <= amountOfBuilder ){
                         Game.creeps[creepName].memory.role = "builder"
+                        Game.creeps[creepName].memory.source = sources[0]
+                    }else if(index <= amountOfBuilder+2){
+                        Game.creeps[creepName].memory.role = "upgrader"
                         Game.creeps[creepName].memory.source = sources[0]
                     }else{
                         Game.creeps[creepName].memory.role = "harvester"
--- src/settings.js
+++ src/settings.js
@@ -2,7 +2,7 @@ let settings = {
     level: 1,
     numberCreeps: 12,
     numberBigCreeps: 3,
-    constructionplaceToBuild: 3
+    constructionplaceToBuild: 4
 };
 
 module.exports = settings;
