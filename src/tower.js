d6fd3e867aab92d8cf729e7eeca501d51fe80be0
Marcel Rösler <roesler1@gmx.de>
2017-04-17 23:22:49 +0200

    tower does attack now

--- src/tower.js
+++ src/tower.js
@@ -2,29 +2,31 @@ let output = require('./output')
 let towerHelper = {
     run: (tower, room)=>{
         let closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS)
-        if(closestHostile) {tower.attack(closestHostile)}
-
-        let damagedContainers = room.find(FIND_STRUCTURES, {
-            filter: (structure) => structure.structureType === STRUCTURE_CONTAINER && structure.hits < structure.hitsMax
-        })
-
-        if(_.size(damagedContainers) > 0){
-            damagedContainers = _.sortByOrder(damagedContainers, ['hits', 'hitsMax'], ['asc', 'asc'])
-            tower.repair(damagedContainers[0])
+        if(closestHostile) {
+            tower.attack(closestHostile)
         }else{
-            let damagedRamparts = room.find(FIND_STRUCTURES, {
-                    filter: (structure) => structure.structureType === STRUCTURE_RAMPART && structure.hits < structure.hitsMax
+            let damagedContainers = room.find(FIND_STRUCTURES, {
+                filter: (structure) => structure.structureType === STRUCTURE_CONTAINER && structure.hits < structure.hitsMax
             })
-            if(_.size(damagedRamparts) > 0){
-                damagedRamparts = _.sortByOrder(damagedRamparts, ['hits', 'hitsMax'], ['asc', 'asc'])
-                tower.repair(damagedRamparts[0])
+
+            if(_.size(damagedContainers) > 0){
+                damagedContainers = _.sortByOrder(damagedContainers, ['hits', 'hitsMax'], ['asc', 'asc'])
+                tower.repair(damagedContainers[0])
             }else{
-                let damagedStructures = room.find(FIND_STRUCTURES, {
-                        filter: (structure) => structure.hits < structure.hitsMax
+                let damagedRamparts = room.find(FIND_STRUCTURES, {
+                    filter: (structure) => structure.structureType === STRUCTURE_RAMPART && structure.hits < structure.hitsMax
                 })
-                if(_.size(damagedStructures) > 0){
-                    damagedStructures = _.sortByOrder(damagedStructures, ['hits', 'hitsMax'], ['asc', 'asc'])
-                    tower.repair(damagedStructures[0])
+                if(_.size(damagedRamparts) > 0){
+                    damagedRamparts = _.sortByOrder(damagedRamparts, ['hits', 'hitsMax'], ['asc', 'asc'])
+                    tower.repair(damagedRamparts[0])
+                }else{
+                    let damagedStructures = room.find(FIND_STRUCTURES, {
+                        filter: (structure) => structure.hits < structure.hitsMax
+                    })
+                    if(_.size(damagedStructures) > 0){
+                        damagedStructures = _.sortByOrder(damagedStructures, ['hits', 'hitsMax'], ['asc', 'asc'])
+                        tower.repair(damagedStructures[0])
+                    }
                 }
             }
         }
