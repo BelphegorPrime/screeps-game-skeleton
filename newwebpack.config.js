fd7f2d9adebd39e882ba51b108a37397db5dc3c7
Marcel Rösler <roesler1@gmx.de>
2017-04-19 23:25:55 +0200

    more testing

---
+++ newwebpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack');
+const path = require('path');
+const TsConfigPathsPlugin = require('awesome-typescript-loader').TsConfigPathsPlugin;
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const chalk = require('chalk');
+
+const SourceDir = path.resolve(__dirname, './src');
+const DistDir = path.join(__dirname, 'dist');
+
+module.exports = {
+    devtool: 'inline-source-map',
+    target: 'node',
+    context: __dirname,
+    entry: {
+        main: path.join(SourceDir, 'main.ts'),
+    },
+    output: {
+        path: DistDir,
+        filename: 'main.js',
+        pathinfo: true,
+        libraryTarget: 'commonjs2',
+    },
+    module: {
+        loaders: [
+            { test: /\.ts/,   loader: 'awesome-typescript-loader' },
+        ]
+    },
+    plugins: [
+        new ProgressBarPlugin({
+            format: '  build [:bar] ' + chalk.green.bold(':percent') + ' (:elapsed seconds)',
+            clear: false,
+        }),
+        new TsConfigPathsPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                BROWSER: JSON.stringify(true),
+                NODE_ENV: JSON.stringify('development')
+            },
+        }),
+        new webpack.NoEmitOnErrorsPlugin(),
+    ],
+};
